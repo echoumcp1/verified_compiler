73559f56d01490b247651ea3e985d4855322ec38
echoumcp1 <101858583+echoumcp1@users.noreply.github.com>
2024-04-20 23:39:41 -0400

    changed names

--- Arith.lean
+++ Arith.lean
@@ -1,4 +1,4 @@
 -- This module serves as the root of the `Arith` library.
 -- Import modules here that should be built as part of the library.
-import «Arith».Basic
+import «Arith».AST
 import «Arith».Example
